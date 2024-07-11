de12347cf3c25ac9ab3091fe4fc719e967443828
Dennis Voltz <dennis.voltz@wwu.de>
2024-07-11 17:17:50 +0200

    Add synopsis view

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 
@@ -21,13 +21,34 @@ const columns = [
 ];
 
 function Search() {
+  const [clickedRow, setClickedRow] = useState(null);
+  const [clickedPage, setClickedPage] = useState(null);
+  const [xmlContent, setXmlContent] = useState('');
+
+  const loadXmlFile = async (pageNumber) => {
+    try {
+      const response = await fetch(`/doc${clickedRow.row.id}/page${pageNumber}.xml`);
+      const text = await response.text();
+      setXmlContent(text);
+    } catch (error) {
+      console.error('Failed to load XML file', error);
+      setXmlContent('Failed to load content');
+    }
+  };
+
   const handleRowClick = (params) => {
-    // params has information about the row clicked
-    console.log(params);
+    setClickedRow(params);
+  };
+
+  const handlePageClick = (event) => {
+    const pageNumber = event.target.getAttribute('data-page');
+    setClickedPage(pageNumber);
+    loadXmlFile(pageNumber);
+    console.log(clickedRow.row.id, pageNumber);
   };
 
   return (
-    <div>
+    <div className='container'>
       <div id="progress-bar" />
       <Box sx={{ height: 400, width: '100%' }}>
         <DataGrid
@@ -46,9 +67,33 @@ function Search() {
           onRowClick={handleRowClick} // Adding the event listener here
         />
       </Box>
+      {clickedRow ? <div>
+        <div>
+          <p>Title: {clickedRow ? clickedRow.row.bookTitle : 'N/A'}</p>
+          <p>Author: {clickedRow ? clickedRow.row.author : 'N/A'}</p>
+          <p>Calligraphy: {clickedRow ? clickedRow.row.calligraphy : 'N/A'}</p>
+          <div>
+            <ul className='page-list'>
+              {clickedRow && clickedRow.row.pages ? (
+                Array.from({ length: clickedRow.row.pages }, (_, i) => i + 1).map((pageNumber) => (
+                  <li onClick={handlePageClick} key={pageNumber} data-page={pageNumber}>{pageNumber}</li>
+                ))
+              ) : (
+                <li onClick={handlePageClick} key={1} data-page={1}>{1}</li>
+              )}
+            </ul>
+          </div>
+
+        </div>
+        <div className='synopsis horizontal'>
+          <div className='left'><img src={`/doc1/page${clickedPage}.jpg`} alt="Manuscript" /></div>
+          <div className='right'><pre>{xmlContent}</pre></div>
+        </div>
+      </div> : null}
+
+
     </div>
   );
 }
-
 
 export default Search;
