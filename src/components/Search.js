34cbb9408d922477b78c789d5126756dd42a40b1
Dennis Voltz <dennis.voltz@wwu.de>
2024-07-11 15:48:50 +0200

    Fix width

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,20 +6,19 @@ import { DataGrid } from '@mui/x-data-grid';
 import manuscripts from '../data/arg_theory.js';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  { field: 'library', headerName: 'Library', width: 250 },
+  { field: 'id', headerName: 'ID', width: 20 },
+  { field: 'library', headerName: 'Library', width: 200 },
   { field: 'bookTitle', headerName: 'Book Title', width: 250 },
   { field: 'englishTranslation', headerName: 'English Translation', width: 250 },
   { field: 'author', headerName: 'Author', width: 250 },
-  { field: 'dateOfBirth', headerName: 'Date of Birth', width: 150, },
-  { field: 'dateOfDeath', headerName: 'Date of Death', width: 150, },
-  { field: 'dateOfCreation', headerName: 'Date of Creation', width: 150 },
-  { field: 'manuscriptType', headerName: 'Manuscript Type', width: 150 },
-  { field: 'inventoryNumber', headerName: 'Inventory Number', width: 150 },
-  { field: 'numberOfPages', headerName: 'Number of Pages', width: 150 },
-  { field: 'calligraphy', headerName: 'Calligraphy', width: 150 },
+  { field: 'dateOfBirth', headerName: 'Date of Birth', width: 100, },
+  { field: 'dateOfDeath', headerName: 'Date of Death', width: 100, },
+  { field: 'dateOfCreation', headerName: 'Date of Creation', width: 100 },
+  { field: 'manuscriptType', headerName: 'Manuscript Type', width: 100 },
+  { field: 'inventoryNumber', headerName: 'Inventory Number', width: 100 },
+  { field: 'numberOfPages', headerName: 'Number of Pages', width: 100 },
+  { field: 'calligraphy', headerName: 'Calligraphy', width: 100 },
 ];
-
 
 function Search() {
   const handleRowClick = (params) => {
