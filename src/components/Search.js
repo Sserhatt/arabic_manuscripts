b3c29ee65b709c3e4c5393ff259ea6edf30110e9
Dennis Voltz <dennis.voltz@wwu.de>
2024-07-11 15:29:12 +0200

    Change data to separate file

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 
-
+// import JSON from file
+import manuscripts from '../data/arg_theory.js';
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 90 },
@@ -17,78 +18,6 @@ const columns = [
   { field: 'inventoryNumber', headerName: 'Inventory Number', width: 150 },
   { field: 'numberOfPages', headerName: 'Number of Pages', width: 150 },
   { field: 'calligraphy', headerName: 'Calligraphy', width: 150 },
-];
-
-const manuscripts = [
-  {
-    id: 1,
-    library: "Diyanet Manuscripts Library/Ankara",
-    bookTitle: "Risālat fī ʿIlm Ādāb al-Baḥth wa-l-Munāẓarah/ والمناظرة رسالة في علم آداب البحث",
-    englishTranslation: "Treatise on the ʿIlm Ādāb al-Baḥth and Debate",
-    author: "Takī ud-Dīn Muhammed ibn Pīr Alī Birgivī / تقي الدین محمد بن پیر علي البركوي",
-    dateOfBirth: 1523,
-    dateOfDeath: 1573,
-    dateOfCreation: "Unknown",
-    manuscriptType: "Text",
-    inventoryNumber: "4780-IV",
-    numberOfPages: "1 folio",
-    calligraphy: "Nastaliq"
-  },
-  {
-    id: 2,
-    library: "Diyanet Manuscripts Library/Ankara",
-    bookTitle: "Risālat fī ʿIlm Ādāb al-Baḥth wa-l-Munāẓarah/  رسالة في علم آداب البحث والمناظرة",
-    englishTranslation: "Treatise on the Science of al-Ādāb al-Baḥth and Debate",
-    author: "Ṭāshkubrīʹzādah, Aḥmad ibn Muṣṭafá /  طاشكبري زاده أحمد بن مصطفي",
-    dateOfBirth: 1495,
-    dateOfDeath: 1561,
-    dateOfCreation: "Unknown",
-    manuscriptType: "Text",
-    inventoryNumber: "6783-V",
-    numberOfPages: "2 folios",
-    calligraphy: "Naskhī"
-  },
-  {
-    id: 3,
-    library: "Diyanet Manuscripts Library/Ankara",
-    bookTitle: "Hāshiyetü’l-Kawākibī 'alā risālati’l-baḥth Ṭāshkubrīʹzādah / حاشیة الكواكبى علي رسالة البحث طاشكبري زاده",
-    author: "Ebüssuûd b. Ahmed b. Muhammed al-Kawākibī al-Hanefī al-Halebī / بن محمد الكواكبى الحنفي الحلبي ابو السعود بن احمد",
-    dateOfBirth: null,
-    dateOfDeath: null,
-    dateOfCreation: "Unknown",
-    manuscriptType: "Commentary",
-    inventoryNumber: "477-V",
-    numberOfPages: "8 folios",
-    calligraphy: "Naskhī"
-  },
-  {
-    id: 4,
-    library: "Diyanet Manuscripts Library/Ankara",
-    bookTitle: "Talhīṣ al-Ḥusaynīyah fī al-Ādāb al-Baḥth wa-l-Munāẓarah / الآدب البحث والمناظرة تلخيص الحسينية في",
-    englishTranslation: "Summary of al-Ḥusaynīyah on the al-Ādāb al-Baḥth and Debate",
-    author: "Muhammad ibn al-Hajj Hamid Mustafa al-Kafawi al-Akirmānī al-Hanafī / حمید مصطفي الكفوي الأقكرماني الحنفي محمد بن الحاج",
-    dateOfBirth: null,
-    dateOfDeath: 1760,
-    dateOfCreation: "Unknown",
-    manuscriptType: "Summary",
-    inventoryNumber: "4027-XXXI",
-    numberOfPages: "4 folios",
-    calligraphy: "Naskhī"
-  },
-  {
-    id: 5,
-    library: "Diyanet Manuscripts Library/Ankara",
-    bookTitle: "Risālat fī al-Ādāb al-Baḥth / رسالة في علم آداب البحث",
-    englishTranslation: "Commentary on the 'Adudiyye Ādāb al-Baḥth",
-    author: "Shams al-Dīn Muḥammad ibn Ashraf al-Ḥusaynī al-Samarqandī / الحسیني السمرقندي شمس الدین محمد ب ن أشرف",
-    dateOfBirth: null,
-    dateOfDeath: 1303,
-    dateOfCreation: "Unknown",
-    manuscriptType: "Original Text",
-    inventoryNumber: "3251-XVI",
-    numberOfPages: "3 folios",
-    calligraphy: "Naskhī"
-  }
 ];
 
 
